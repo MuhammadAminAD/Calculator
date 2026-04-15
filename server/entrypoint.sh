3eedb605254dcfc61be66e5f0f324d17b4f06079
Izzatillo <izzatillaasatullayev55@gmail.com>
2026-04-15 15:47:53 +0500

    add : some changes in entrypoint.sh

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -1,3 +1,19 @@
 #!/bin/sh
 
+set -e
+
+echo 
+
+if [ -n "$DB_HOST" ] && [ -n "$DB_PORT" ]; then
+  echo "⏳ Database kutilyapti: $DB_HOST:$DB_PORT"
+
+  while ! nc -z $DB_HOST $DB_PORT; do
+    sleep 1
+  done
+
+  echo 
+fi
+
+echo 
+
 exec "$@"
